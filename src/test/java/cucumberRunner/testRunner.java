fcd35a2a872179cf2767cc27f89dbfbc654922cd
User <User@192.168.1.10>
2019-05-29 22:07:31 +0700

    update

--- src/test/java/cucumberRunner/testRunner.java
+++ src/test/java/cucumberRunner/testRunner.java
@@ -1,11 +1,8 @@
 package cucumberRunner;
 
-import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", glue = "stepDefine")
 public class testRunner {
 
