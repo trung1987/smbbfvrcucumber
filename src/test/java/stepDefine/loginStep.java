2e6b821184f47c964b603ea5e175df55a4c43834
User <User@192.168.1.10>
2019-06-02 10:09:53 +0700

    re-up

--- src/test/java/stepDefine/loginStep.java
+++ src/test/java/stepDefine/loginStep.java
@@ -2,26 +2,21 @@ package stepDefine;
 
 import java.io.File;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import Actions.LoginAction;
-import Interface.LoginInterface;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import utils.Method;
+import utils.BasedTest;
 
-public class loginStep {
+public class loginStep extends BasedTest{
 
-	static WebDriver driver;
 	LoginAction login;
 
 	@Given("^Open Chrome and navigate$")
 	public void open_Chrome_and_navigate() throws Throwable {
 		System.setProperty("webdriver.chrome.driver", "." + File.separator + "libs" + File.separator + "chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.manage().window().maximize();
 		login = new LoginAction(driver);
 		login.openBrowser();
 	}
@@ -40,22 +35,22 @@ public class loginStep {
 	@Then("^I login success$")
 	public void i_login_success() throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
-		
-		Method method = new Method();
+		driver.quit();
+		//Method method = new Method();
 
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		if (method.checkExist(driver, LoginInterface.cusName)) {
-
-			System.out.println("SUCCESS");
-		} else {
-
-			System.out.println("FAIL");
-		}
+//		try {
+//			Thread.sleep(5000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		if (method.checkExist(driver, LoginInterface.cusName)) {
+//
+//			System.out.println("SUCCESS");
+//		} else {
+//
+//			System.out.println("FAIL");
+//		}
 	}
 
 }
